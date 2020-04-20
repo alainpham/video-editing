3387908379dc9c5c53fe0a4e1139b50865cadba5
Alain Pham <apham@redhat.com>
2020-04-20 02:02:09 +0200

    changed ardour with zoom second mic

--- jackconfig/scripts/sound-live-podcast-solo.sh
+++ jackconfig/scripts/sound-live-podcast-solo.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 cd $(dirname $0)
 echo "Shutting down all existing jackd and alsa_in"
-killall -9 jackd >/dev/null 2>&1
+#killall -9 jackd >/dev/null 2>&1
 killall -9 alsa_in >/dev/null 2>&1
 killall -9 alsa_out >/dev/null 2>&1
 sleep 2
-echo "Starting jack"
-nohup jackd -dalsa -dhw:USB -r44100 -p1024 -n2 </dev/null >/dev/null 2>&1 &
-sleep 10
-echo "Restart pulseaudio"
-pulseaudio -k
+#echo "Starting jack"
+#nohup jackd -dalsa -dhw:USB -r44100 -p1024 -n2 </dev/null >/dev/null 2>&1 &
+#sleep 10
+#echo "Restart pulseaudio"
+#pulseaudio -k
 sleep 10
 echo "Start alsa_out for dock"
 nohup alsa_out -j dock -d hw:PCH -r 44100 </dev/null >/dev/null 2>&1 &
+echo "Start alsa_in for H1 mic"
+nohup alsa_in -j H1-in -d hw:H1 -r 44100 </dev/null >/dev/null 2>&1 &
+echo "Start alsa_out for H1 mic"
+nohup alsa_out -j H1-out -d hw:H1 -r 44100 </dev/null >/dev/null 2>&1 &
 sleep 2
 nohup ardour5 live-podcast </dev/null >/dev/null 2>&1 &
