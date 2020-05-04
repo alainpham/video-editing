0464bc94ec99934f320441461a57593772937ff6
Alain Pham <apham@redhat.com>
2020-05-05 00:01:28 +0200

    new eq

--- c920-config/cam-artificial.sh
+++ c920-config/cam-artificial.sh
@@ -1,23 +1,17 @@
+
+cam_list="video0 video2 video4"
+
 chan=video2
+for chan in $cam_list
+do
+	v4l2-ctl -d /dev/$chan -c exposure_auto=1
+	v4l2-ctl -d /dev/$chan -c exposure_absolute=300
+	v4l2-ctl -d /dev/$chan -c gain=40
+	v4l2-ctl -d /dev/$chan -c sharpness=150 
+	v4l2-ctl -d /dev/$chan -c white_balance_temperature_auto=0
+	v4l2-ctl -d /dev/$chan -c white_balance_temperature=3400 
+	v4l2-ctl -d /dev/$chan -c focus_auto=0 
+	v4l2-ctl -d /dev/$chan -c focus_absolute=0 
+	v4l2-ctl -d /dev/$chan -c power_line_frequency=1
+done
 
-v4l2-ctl -d /dev/$chan -c exposure_auto=1
-v4l2-ctl -d /dev/$chan -c exposure_absolute=300
-v4l2-ctl -d /dev/$chan -c gain=40
-v4l2-ctl -d /dev/$chan -c sharpness=150 
-v4l2-ctl -d /dev/$chan -c white_balance_temperature_auto=0
-v4l2-ctl -d /dev/$chan -c white_balance_temperature=3400 
-v4l2-ctl -d /dev/$chan -c focus_auto=0 
-v4l2-ctl -d /dev/$chan -c focus_absolute=0 
-v4l2-ctl -d /dev/$chan -c power_line_frequency=1
-
-chan=video4
-
-v4l2-ctl -d /dev/$chan -c exposure_auto=1
-v4l2-ctl -d /dev/$chan -c exposure_absolute=300
-v4l2-ctl -d /dev/$chan -c gain=40
-v4l2-ctl -d /dev/$chan -c sharpness=150 
-v4l2-ctl -d /dev/$chan -c white_balance_temperature_auto=0
-v4l2-ctl -d /dev/$chan -c white_balance_temperature=3400 
-v4l2-ctl -d /dev/$chan -c focus_auto=0 
-v4l2-ctl -d /dev/$chan -c focus_absolute=0 
-v4l2-ctl -d /dev/$chan -c power_line_frequency=1
