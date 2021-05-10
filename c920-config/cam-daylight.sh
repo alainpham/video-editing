08e01c2a3b63248cc189f26c5e0f73368719d55f
Alain Pham <alainphm@gmail.com>
2021-05-10 17:26:48 +0200

    template odt

--- c920-config/cam-daylight.sh
+++ c920-config/cam-daylight.sh
@@ -5,7 +5,7 @@ for chan in $cam_list
 do
 v4l2-ctl -d /dev/$chan -c exposure_auto=1
 v4l2-ctl -d /dev/$chan -c exposure_absolute=300
-v4l2-ctl -d /dev/$chan -c gain=0
+v4l2-ctl -d /dev/$chan -c gain=20
 v4l2-ctl -d /dev/$chan -c sharpness=150 
 v4l2-ctl -d /dev/$chan -c white_balance_temperature_auto=0
 v4l2-ctl -d /dev/$chan -c white_balance_temperature=5500
