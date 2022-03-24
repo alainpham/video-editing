19a87dea76a7afdb24ae39117cdaaac06cd8fe89
apham <apham@work.work.lan>
2022-03-24 12:38:28 +0100

    fix rendering scripts

--- ffmpeg-rendering-scripts/vconv-x264-vbr-2pass.sh
+++ ffmpeg-rendering-scripts/vconv-x264-vbr-2pass.sh
@@ -12,7 +12,7 @@ x264yifigist=cabac=1:ref=5:analyse=0x133:me=umh:subme=9:chroma-me=1:deadzone-int
 
 x264paramsrarbg=cabac=1:ref=4:deblock=-1,-1:analyse=0x3,0x113:me=umh:subme=9:psy-rd=1.00,0.15:merange=24:trellis=2:deadzone-inter=21:deadzone-intra=11:chroma-qp-offset=0:threads=32:lookahead-threads=4:sliced-threads=0:sync-lookahead=2:nr=0:bluray-compat=0:constrained-intra=0:bframes=3:b-pyramid=2:b-adapt=2:b-bias=0:direct=auto:open-gop=0:weightp=2:keyint=250:min-keyint=25:scenecut=40:intra-refresh=0:rc-lookahead=50:qpmin=0:qpmax=69:qpstep=4:cplxblur=20.0:qblur0.5:vbv-maxrate=31250:vbv-bufsize=31250:aq-mode=3:partitions=all
 
-x264minimal=cabac=1:ref=5:deblock=-1,-1:me=umh:subme=9:psy-rd=1.00,0.15:merange=24:trellis=2:chroma-qp-offset=-1:bframes=5:b-adapt=2:direct=auto:rc-lookahead=60:vbv-maxrate=15000:vbv-bufsize=15000:aq-mode=3:partitions=all
+x264minimal=cabac=1:ref=5:deblock=-1,-1:me=umh:subme=9:psy-rd=1.00,0.15:merange=24:trellis=2:chroma-qp-offset=0:bframes=5:b-adapt=2:direct=auto:rc-lookahead=60:vbv-maxrate=10000:vbv-bufsize=10000:aq-mode=3:partitions=all
 
 
 x264params=$x264minimal
